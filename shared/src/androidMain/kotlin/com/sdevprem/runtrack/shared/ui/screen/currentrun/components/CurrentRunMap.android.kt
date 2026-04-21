c431e0fe62efd5e06cb02d305909910d73579cef
lanmuz <38832461+lanmuz@users.noreply.github.com>
2026-04-21 22:02:45 +0800

    fixshabi

--- shared/src/androidMain/kotlin/com/sdevprem/runtrack/shared/ui/screen/currentrun/components/CurrentRunMap.android.kt
+++ shared/src/androidMain/kotlin/com/sdevprem/runtrack/shared/ui/screen/currentrun/components/CurrentRunMap.android.kt
@@ -138,7 +138,7 @@ private fun RenderMapContent(
             isMyLocationEnabled = true,
             myLocationStyle = MyLocationStyle().apply {
                 myLocationIcon(
-                    MapUtils.bitmapDescriptorFromVector(
+                    AppMapUtils.bitmapDescriptorFromVector( //▲▲▲▲▲▲▲▲
                         context = context,
                         vectorResId = R.drawable.ic_location_marker,
                         tint = Color.Blue.toArgb(),
@@ -189,8 +189,8 @@ private fun RenderMapContent(
     var locationError by remember { mutableStateOf<String?>(null) }
     var showGpsDialog by remember { mutableStateOf(false) }
     LaunchedEffect(Unit) {
-        MapUtils.setMapPrivacy(context, true, true) //▲▲▲▲▲▲▲▲
-        if (!MapUtils.isMapPrivacyAgreed(context)) { //▲▲▲▲▲▲▲▲
+        MapUtils.setMapPrivacy(context, true) //▲▲▲▲▲▲▲▲
+        if (!MapUtils.isMapPrivacyAgreed()) { //▲▲▲▲▲▲▲▲
             locationError = "高德地图隐私协议未同意"
         }
     }
@@ -345,7 +345,7 @@ private fun TakeScreenShot(
     val mapApplier = currentComposer.applier as? MapApplier
     LaunchedEffect(take, mapApplier, mapCenter, mapSize) {
         if (take && mapApplier?.map != null) {
-            AppMapUtils.takeSnapshot( //▲▲▲▲▲▲▲▲
+            AppMapUtils.takeSnapshot(
                 context = context,
                 mapApplier.map,
                 pathPoints,
@@ -364,8 +364,8 @@ private fun DrawPathPoints(
     isRunningFinished: Boolean,
 ) {
     val context = LocalContext.current
-    val lastMarkerState = rememberMarkerState<LatLng>() //▲▲▲▲▲▲▲▲
-    val largeLastMarkerState = rememberMarkerState<LatLng>() //▲▲▲▲▲▲▲▲
+    val lastMarkerState = rememberMarkerState() //▲▲▲▲▲▲▲▲
+    val largeLastMarkerState = rememberMarkerState() //▲▲▲▲▲▲▲▲
     val lastLocationPoint by remember(pathPoints) {
         derivedStateOf { pathPoints.lasLocationPoint() }
     }
